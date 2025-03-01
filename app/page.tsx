03f8dcafc1aa5d6e87d61d3b82033d7a35dfa933
NIZRHM <76219204+nizrhm@users.noreply.github.com>
2025-03-01 11:30:46 +0530

    1/3 -1

--- app/page.tsx
+++ app/page.tsx
@@ -26,30 +26,7 @@ export default function Page() {
       <Header />
       <main>
         <Hero />
-        <Features />
-        <Section
-          leftHalf={
-            <>
-              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">
-                Effortlessly highlight the key features of your app
-              </h2>
-              <p className="text-xl font-light">
-                Our app makes it easy to showcase your key features. With customizable sections, you can highlight the
-                most important aspects of your product. More to come.
-              </p>
-            </>
-          }
-          rightHalf={
-            <Image src={"/products/phone.png"} alt="section-image" width={500} height={100} className="w-1/2 h-auto" />
-          }
-        />
         <Customers />
-        <div className="relative flex items-center justify-center h-screen bg-black">
-          <div className="relative z-10 text-white text-center">
-            <h1 className="text-5xl font-bold">Welcome to My Website</h1>
-            <p className="text-xl mt-4">A futuristic experience with three.js</p>
-          </div>
-        </div>
         <Section
           leftHalf={<Accordion />}
           rightHalf={
