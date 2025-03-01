888bffd599aebab7742ffd95a5d27aa64e160a87
NIZRHM <76219204+nizrhm@users.noreply.github.com>
2025-03-01 11:34:45 +0530

    1/3 - 2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <section className="text-center my-32 mx-4 sm:mx-8 md:mx-16 lg:mx-24">
       <h1 className="font-sans text-3xl tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-        NextJS Template amazing hero!
+        Your SKIN - Our Science!
       </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
-        Add something about your saas service or app here.
+        Quality Skincare Everywhere.
       </p>
       <div className="flex justify-center space-x-6">
         <Link
