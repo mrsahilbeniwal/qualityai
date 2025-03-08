f7b8303519b4232741ab11621c2b5a45f2741ba0
NIZRHM <76219204+nizrhm@users.noreply.github.com>
2025-03-08 15:19:39 +0530

    front page changes

--- components/Lamp.tsx
+++ components/Lamp.tsx
@@ -15,7 +15,7 @@ export default function LampDemo() {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Build lamps <br /> the right way
+        Building solutions <br /> that light your way
       </motion.h1>
     </LampContainer>
   )
