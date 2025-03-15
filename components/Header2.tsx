2267acbad660cf9f994be52dcf3aba6609dab395
NIZRHM <76219204+nizrhm@users.noreply.github.com>
2025-03-15 12:00:45 +0530

    header changes

--- components/Header2.tsx
+++ components/Header2.tsx
@@ -1,19 +1,12 @@
 import Link from "next/link";
 import ThemeSwitch from "./ThemeSwitch";
-import Image from "next/image"; // Import Image component for optimized image rendering
 
 export default function Header() {
   return (
     <header className="bg-white dark:bg-black shadow-sm dark:border-b dark:border-gray-800">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="flex items-center justify-center flex-grow">
-          {/* Logo */}
-          <Image 
-            src="public/assets/qai.png" // Path to the logo image in the public folder
-            alt="QualityAI Logo"
-            width={150} // Adjust width as necessary
-            height={40} // Adjust height as necessary
-          />
+        <div className="flex items-center">
+          <span className="text-2xl dark:text-gray-100">QualityAI</span>
         </div>
         <nav className="flex items-center">
           <ul className="flex space-x-2 mr-2">
@@ -22,7 +15,7 @@ export default function Header() {
                 href="/contact"
                 className="text-sm text-gray-800 dark:text-white px-4 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
               >
-                Connect
+                Contact
               </Link>
             </li>
           </ul>
